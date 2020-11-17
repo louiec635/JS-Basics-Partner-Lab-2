05f8a3eac46e41919deed7cc1c5196ff7ab69a38
Kalyn Smith <kalyn@Kalyns-MacBook-Air.local>
2020-11-17 16:37:56 -0500

    finished product

--- main.js
+++ main.js
@@ -9,9 +9,24 @@
 // }
 
 // Step 2: Ask user for area of circle.
-let userRadius = prompt("Give a radius");
-if (userRadius < 0) {
-  alert("The radius must not be a negative number");
+// let userRadius = prompt("Give a radius");
+// if (userRadius < 0) {
+//   alert("The radius must not be a negative number");
+// }
+// let area = Math.PI * Math.pow(userRadius, 2);
+// alert("The area of the circle is " + area);
+
+let weight=prompt("Enter weight in lbs");
+let height=prompt("Enter height inches");
+let bmi =weight/Math.pow(height,2); 
+if (bmi<18.5){
+    alert("BMI: "+bmi+ " Underweight")
+} else if (bmi>=18.5 && bmi<=24.9){
+    alert("BMI: "+bmi+ " Normal" )
+} else if (bmi>=25 && bmi<=29.9){
+    alert("BMI:" +bmi+ "Overweight")
+} else { 
+    alert("BMI: "+bmi+ " Obese")
 }
-let area = Math.PI * Math.pow(userRadius, 2);
-alert("The area of the circle is " + area);
+
+
